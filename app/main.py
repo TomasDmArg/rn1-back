89da55f739806363fe03cf1aa8d61176cef9d20c
Tomas <info@tmdm.com.ar>
2024-10-20 17:45:32 -0300

    [fix] remove cors restrictions

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust according to your frontend
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
